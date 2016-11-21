9b5e72d106a2479c9408a6bc603b53088966f3fd
languid <languid.jeremy@gmail.com>
2016-11-21 13:50:51 +0800

    commit

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,21 @@
  * Created by Yinxiong on 2016/11/21.
  */
 
+import babel from 'rollup-plugin-babel';
+
 export default {
     entry: 'index.js',
     dest: 'build/helper.js',
     moduleName: 'helper',
     format: 'umd',
-    globals: {
-        lodash: '_',
-        jquery: '$'
-    }
+	external: ['lodash', 'jquery'],
+	globals: {
+    	lodash: '_',
+		jquery: '$'
+	},
+	plugins: [
+		babel({
+			exclude: 'node_modules/**'
+		})
+	]
 }
